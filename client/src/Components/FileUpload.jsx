1b2ece3f62eed456ed83327092ad564808654aa5
Carson Craft <ccraft4444@gmail.com>
2023-04-10 15:52:54 -0700

    pdf reader pulls text, credit system

--- client/src/Components/FileUpload.jsx
+++ client/src/Components/FileUpload.jsx
@@ -1,34 +1,71 @@
+import axios from "axios";
+import { useState } from "react";
+import useAuth from "../hooks/useAuth";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 export default function FileUploader() {
-  const fileInput = document.getElementById("pdfFileInput");
+  const { fetchMe, updateCredits, selectedUser, setUser } = useAuth();
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [text, setText] = useState("");
+  const navigate = useNavigate();
 
-  fileInput.addEventListener("change", async (event) => {
-    const file = event.target.files[0];
-    const reader = new FileReader();
-
-    reader.onload = async (event) => {
-      const arrayBuffer = event.target.result;
-      const text = await extractPdfText(arrayBuffer);
-      console.log(text);
-    };
-
-    reader.readAsArrayBuffer(file);
-  });
-
-  const extractPdfText = async (arrayBuffer) => {
-    const pdf = await pdfjsLib.getDocument({ data: arrayBuffer }).promise;
-    let text = "";
-
-    for (let i = 1; i <= pdf.numPages; i++) {
-      const page = await pdf.getPage(i);
-      const content = await page.getTextContent();
-      text += content.items.map((item) => item.str).join(" ");
+  useEffect(() => {
+    if (selectedUser) {
+      console.log("Selected user updated:", selectedUser);
     }
+  }, [selectedUser]);
 
-    return text;
+  const handleFileInputChange = (event) => {
+    setSelectedFile(event.target.files[0]);
   };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    if (!selectedFile) {
+      return;
+    }
+    if (selectedUser.credits > 1) {
+      const newTotalCredits = selectedUser.credits - 1;
+      const newCredits = await updateCredits({ credits: newTotalCredits });
+      setUser({ ...selectedUser, credits: newCredits });
+      console.log("selected user", selectedUser);
+
+      const formData = new FormData();
+      formData.append("pdfFile", selectedFile);
+
+      fetch("/routes/documents/extract-text", {
+        method: "POST",
+        body: formData,
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error(response.statusText);
+          }
+          return response.text();
+        })
+        .then((extractedText) => {
+          setText(extractedText.trim());
+        })
+        .catch((error) => {
+          console.error(error);
+          setText("Error extracting text");
+        });
+    } else {
+      setTimeout(() => {
+        navigate("/purchase");
+      }, 0);
+    }
+  };
+
   return (
     <div>
-      <input type="file" id="pdfFileInput" />
+      <form onSubmit={handleFormSubmit}>
+        <input type="file" onChange={handleFileInputChange} />
+        <button type="submit">Upload</button>
+      </form>
+      <textarea value={text} readOnly></textarea>
     </div>
   );
 }
