e7148f8be19d564b620bd10ea0b216d74be0d231
Carson Craft <ccraft4444@gmail.com>
2023-04-20 16:05:43 -0700

    added instructions/small fixes

--- client/src/Components/Response.jsx
+++ client/src/Components/Response.jsx
@@ -3,6 +3,25 @@ export default function Response({ response }) {
     <>
       <h1>Here are your study tools</h1>
       <p>{response}</p>
+
+      <h1>Upload to quizlet</h1>
+      <ls>
+        <li>Copy your study tools</li>
+        <li>
+          Go to{" "}
+          <a
+            href="https://www.quizlet.com/create-set"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            quizlet.com/create-set
+          </a>
+        </li>
+        <li>Click the + import button</li>
+        <li>Paste your copied tools</li>
+        <li>Click the Comma button bellow your data</li>
+        <li>Click the import button</li>
+      </ls>
     </>
   );
 }
